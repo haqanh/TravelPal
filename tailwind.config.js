b8c485fa9688ba6a1571dcee5bfd6ce8139dcde8
Tan Xin Li Terri <territanxinli123@gmail.com>
2024-03-16 03:35:19 +0800

    Add Trip Modals

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#3F3D3D',
+        'secondary': '#91959D',
+      },
+
+      fontFamily: {
+        'sans': ['Poppins', 'sans-serif'],
+      },
+
+      container: {
+        padding: "2rem",
+        center: true,
+      },
+
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+      }
+    }
   },
   plugins: [],
 }
