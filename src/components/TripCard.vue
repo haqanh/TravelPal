692e5b6e6a3762c72ef1a43ad5385ed1f1118836
Tan Xin Li Terri <territanxinli123@gmail.com>
2024-03-22 17:40:50 +0800

    Standardisation of Dashboard Cards

--- src/components/TripCard.vue
+++ src/components/TripCard.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="flex items-center justify-center min-h-screen">
-        <div class="relative max-w-[200px] h-[240px] bg-white border border-gray-200 rounded-[20px] shadow shadow-xl dark:bg-gray-800 dark:border-gray-700">
-            <img class="rounded-t-[20px] h-[180px] w-[200px] shadow-l object-cover object-bottom" src="../assets/Trip Image.jpg" alt="" />
+        <div class="relative max-w-[200px] h-[245px] bg-white border border-gray-200 rounded-[20px] custom-shadow dark:bg-gray-800 dark:border-gray-700">
+            <img class="rounded-t-[20px] h-[180px] w-[200px] custom-shadow object-cover object-bottom" src="../assets/Trip Image.jpg" alt="" />
             <div class="absolute top-0 right-0 mt-2 mr-2 flex items-center">
                 <img class="h-3 w-3 relative" src="../assets/map-pin.svg" alt="" />
-                <span class="ml-1 font-medium text-sky-50 text-[10px] text-white">Taranaki, New Zealand</span>
+                <span class="ml-1 font-medium text-sky-50 text-[10px] text-[#E1F1FF]">Taranaki, New Zealand</span>
             </div>
             <div class="p-2">
                 <div class="flex justify-between items-center">
@@ -16,7 +16,7 @@
                         <img class="mb-2 h-4" src="../assets/Union.svg" alt="" />
                     </div>
                 </div>
-                <p class="pb-1 text-[10px] font-medium text-[#3F3D3D] text-opacity-60">Sep 2 ‘20 - Sep 28 ‘20</p>
+                <p class="pt-1 text-[10px] font-medium text-[#3F3D3D] text-opacity-60">Sep 2 ‘20 - Sep 28 ‘20</p>
             </div>
         </div>
     </div>
@@ -25,5 +25,9 @@
 <style scoped>
 .object-bottom {
     object-position: bottom;
+}
+
+.custom-shadow {
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 }
 </style>
