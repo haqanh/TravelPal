8fec20a40a8549c0dac416fdca5cbe9c14196685
Tan Xin Li Terri <territanxinli123@gmail.com>
2024-04-06 03:22:37 +0800

    Include Sidebar.vue

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -3,6 +3,7 @@
       <NavBar />
     </div>
     <div class="flex">
+      <SideBar />
       <div class="w-80vw p-10 my-10 mx-10">
         <div class="DashboardContents flex flex-col lg:flex-row justify-between lg:items-center -mx-10">
             <h1 class="text-[60px] font-semibold text-[#3F3D3D] mx-10">Home</h1>
