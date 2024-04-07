d3c6e8c89a5a67e38ce550a3e7ab76df6bb77060
Jovan131 <122272142+Jovan131@users.noreply.github.com>
2024-04-07 17:10:24 +0800

    Allow adding of profile pic in user profiling

--- src/components/CountryCheckbox.vue
+++ src/components/CountryCheckbox.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="selectedCountries.length >= 11 && !selectedCountries.includes(country)" class="flex has-[:checked]:bg-blue-100 cursor-not-allowed">
+  <div v-if="selectedCountries.length >= 10 && !selectedCountries.includes(country)" class="flex has-[:checked]:bg-blue-100 cursor-not-allowed">
     <input disabled @click.prevent @click="$emit('toggle-country', $event.target.value)" :checked="selectedCountries.includes(country)" type="checkbox" :id="'checkbox' + country" :value="country" class="self-start mt-[5px] cursor-not-allowed" />
     <label :for="'checkbox' + country" class="ml-2 text-left w-full select-none cursor-not-allowed">{{ country }}</label>
   </div>
