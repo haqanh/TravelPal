5e6a67b36e2fdf9f7d4799f800872c2d0151a189
haqanh <quanganhha23@gmail.com>
2024-04-25 03:44:53 +0800

    fix typescript errors and google map types

--- src/router/index.ts
+++ src/router/index.ts
@@ -23,17 +23,6 @@ const router = createRouter({
       component: LandingPage
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-      meta: {
-        requiresAuth: true
-      }
-    },
-    {
       path: '/add-trip',
       name: 'AddTrip',
       component: AddTrip,
